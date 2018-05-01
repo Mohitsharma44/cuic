d13f07d6d47b3bc50f9fc01afcc5d0a88b0a80a7
Mohitsharma44 <mohitsharma44@gmail.com>
2018-04-30 23:27:58 -0400

    working version of watchdog that uses xmlrpclib to communicate with supervisorctl to start and stop cuiccapture

--- using_teledyne/capture_watchdog/watchdog.py
+++ using_teledyne/capture_watchdog/watchdog.py
@@ -7,8 +7,10 @@ import pika
 import logger
 import asyncio
 import pyinotify
+import threading
 import multiprocessing
 from rpc_client import UOControllerRpcClient
+from xmlrpc.client import ServerProxy
 
 logger = logger.logger(tofile=False)
 
@@ -33,27 +35,71 @@ known_vis_queues = {
     "370Roof"  : {"cam": "370Roof", "watchdog": "370Roof_watchdog"},
     "test"     : {"cam": "test", "watchdog": "test_watchdog"},
 }
-
+proxy = ServerProxy('http://127.0.0.1:9001/RPC2')
+RUNNING_STATENAME     = ["RUNNING"]
+NOT_RUNNING_STATENAME = ["STARTING", "BACKOFF",
+                         "FATAL", "UNKNOWN"]
+STOPPED_STATENAME     = ["STOPPING", "STOPPED", "EXITED"]
 
 class FsEventHandler(pyinotify.ProcessEvent):
     def __init__(self, cam_commands):
-        self._prev_timestamp = 0
         self._current_timestamp = 0
         self.cam_commands = cam_commands
-
+        self.check_timestamp()
+        self.BUSY = False
+        
+    def check_timestamp(self):
+        """
+        Function to check the timestamp and if a drift of 
+        > 3*interval requested is observed, it makes a call
+        to restart the cuiccapture code.
+        .. Note: Its a super Ugly way for checking timestamp of the files
+        and comparing them. This method creates new thread object
+        everytime its called. Hate this implementation
+        but cannot find a better alternative. Maybe for some othertime or ... decade 
+        """
+        try:
+            if self.cam_commands.get("capture", 0) != 0 and self.BUSY == False:
+                if proxy.supervisor.getProcessInfo("cuiccapture")['statename'] not in RUNNING_STATENAME:
+                    self.restart_camera()
+                    # Give camera a little time to start capturing
+                    self._current_timestamp = time.time()
+                if time.time() - self._current_timestamp > 3*(cam_commands["interval"]):
+                    logger.warning("No Images received in last {} seconds".\
+                                   format(3*(cam_commands["interval"])))
+                    self.restart_camera()
+            else:
+                # watchdog shouldn't restart the code next time the camear is instructed
+                # to capture, so update the _current_timestamp
+                # to current time
+                self._current_timestamp = time.time()
+        except Exception as ex:
+            logger.error("Error in check_timestamp: "+str(ex))
+        finally:
+            threading.Timer(2, self.check_timestamp).start()
+            
+    def restart_camera(self):
+        try:
+            self.BUSY = True
+            logger.warning("Restarting cuicucapture code")
+            if proxy.supervisor.getProcessInfo("cuiccapture")['statename'] not in STOPPED_STATENAME:
+                proxy.supervisor.stopProcess("cuiccapture")
+                time.sleep(10)
+            if proxy.supervisor.getProcessInfo("cuiccapture")['statename'] not in RUNNING_STATENAME:
+                proxy.supervisor.startProcess("cuiccapture")
+                time.sleep(10)
+            vis_cam_rpc_client = UOControllerRpcClient(vhost=RPC_VHOST,
+                                                       queue_name=known_vis_queues[self.cam_commands["location"]]["cam"])
+            vis_cam_response = vis_cam_rpc_client.call(json.dumps(dict(self.cam_commands)))
+        except Exception as ex:
+            logger.error("Exception in restart_camera: "+str(ex))
+        finally:
+            self.BUSY = False
+            
     def process_IN_CREATE(self, event):
         try:
             print("Creating:", event.pathname)
             self._current_timestamp = os.path.getmtime(event.pathname)
-            while True:
-                if time.time() - self._current_timestamp > 2*(cam_commands["interval"]):
-                    print("will restart because no images received in last 2*interval")
-                    vis_cam_rpc_client = UOControllerRpcClient(vhost=RPC_VHOST,
-                                                               queue_name=known_vis_queues[cam_commands["location"]]["cam"])
-                    cam_response = vis_cam_rpc_client.call(json.dumps(dict(cam_commands)))
-                    break
-                time.sleep(0.5)
-            self._prev_timestamp = self._current_timestamp
         except Exception as ex:
             logger.warning("Exception in processing IN_CREATE event: "+str(ex))
         
@@ -104,8 +150,12 @@ def _store_commands(command_dict, cam_commands):
         "stop": bool(command_dict["stop"]),
         "aperture": int(command_dict["aperture"]),
     }
-    cam_commands.update(new_command)
-    print("Stored ==> ", cam_commands)
+    # Only update the command when status != True. i.e
+    # we don't want to store the command if the controller
+    # has sent request to just get the status of the camera
+    if not new_command.get("status", False):
+        cam_commands.update(new_command)
+        print("Stored ==> ", cam_commands)
     
 def on_rpc_request(ch, method, props, body):
     """

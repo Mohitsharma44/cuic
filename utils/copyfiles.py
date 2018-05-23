b40c1ae074f9493adf23f4f5eb13ffa0d515cbb5
YukunVVan <31962749+YukunVVan@users.noreply.github.com>
2018-05-23 02:16:43 -0400

    sched

--- utils/copyfiles.py
+++ utils/copyfiles.py
@@ -1,34 +1,55 @@
 import os
-# import time
 from datetime import datetime
 import shutil
 from filecmp import cmp
+import sched
+import time
 
-base = "/nfs-volume/volume5/HTI/1mtcNorth"
-path = base+"/live"
+def copyfile():
+    base = "/nfs-volume/volume5/HTI/1mtcNorth"
+    path = base+"/live"
 
-log = open('copy_log.log', 'w')
-for f in os.listdir(path):
-    #Generate new directory path
-    file = os.path.join(path, f)
-    time = datetime.fromtimestamp(os.stat(file).st_mtime)
-    npath = base + "/%d/%02d/%02d/%02d"%(time.year,time.month,time.day,time.hour)
-    nfile = os.path.join(npath, f)
-    log.write('check '+nfile+'\n')
+    tlog = datetime.now().isoformat()
+    # print('log: '+tlog)
+    log = open('copylog_'+tlog+'.log', 'w')
+    for f in os.listdir(path):
+        #Generate new directory path
+        file = os.path.join(path, f)
+        time = datetime.fromtimestamp(os.stat(file).st_mtime)
+        npath = base + "/%d/%02d/%02d/%02d"%(time.year,time.month,time.day,time.hour)
+        nfile = os.path.join(npath, f)
+        log.write('check '+nfile+'\n')
 
-    #Create the directory if not exist
-    if not os.path.exists(npath):
-        log.write('create dir: '+npath+'\n')
-        os.makedirs(npath)
+        #Create the directory if not exist
+        if not os.path.exists(npath):
+            log.write('create dir: '+npath+'\n')
+            os.makedirs(npath)
 
-    if not os.path.exists(nfile):
-        #Copy the file
-        shutil.copy2(file, npath)
-        log.write('copy to '+npath+'\n')
-        #Compare the file
-        log.write('compare result: '+str(cmp(file,nfile))+'\n')
-    else:
-        log.write('already exist: '+nfile+'\n')
+        if not os.path.exists(nfile):
+            #Copy the file
+            shutil.copy2(file, npath)
+            log.write('copy to '+npath+'\n')
+            #Compare the file
+            log.write('compare result: '+str(cmp(file,nfile))+'\n')
+        else:
+            log.write('already exist: '+nfile+'\n')
 
-log.close()
-sys.exit()
+    log.close()
+#     sys.exit()
+
+class PeriodicScheduler(object):
+    def __init__(self):
+        self.scheduler = sched.scheduler(time.time, time.sleep)
+
+    def setup(self, interval, action, actionargs=()):
+        action(*actionargs)
+        self.scheduler.enter(interval, 1, self.setup,
+                        (interval, action, actionargs))
+
+    def run(self):
+        self.scheduler.run()
+
+INTERVAL = 60 # every minute
+periodic_scheduler = PeriodicScheduler()
+periodic_scheduler.setup(INTERVAL, copyfile) # it executes the event just once
+periodic_scheduler.run() # it starts the scheduler

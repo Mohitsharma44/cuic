e0c21dcfe0182b7c1e860b4c8832d64ad383527e
YukunVVan <31962749+YukunVVan@users.noreply.github.com>
2018-06-03 13:22:29 -0400

    notifier

--- utils/tutorial_notifier.py
+++ utils/tutorial_notifier.py
@@ -4,18 +4,28 @@
 #
 import pyinotify
 
+# path = "/nfs-volume/volume5/HTI/1mtcNorth"
+path = './mov'
 wm = pyinotify.WatchManager()  # Watch Manager
 mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE  # watched events
 
 class EventHandler(pyinotify.ProcessEvent):
+    def gettemp(f):
+        file = open(f,'r')
+        return file.read() 
+
     def process_IN_CREATE(self, event):
-        print("Creating: {}".format(event.pathname))
+        nf = event.pathname.split('.')[0]+'.temp'
+        print("Creating: {}".format(nf))
+        t = open(nf, 'w')
+        t.write(gettemp(event.pathname))
+        t.close()
 
     def process_IN_DELETE(self, event):
         print("Removing: {}".format(event.pathname))
 
 handler = EventHandler()
 notifier = pyinotify.Notifier(wm, handler)
-wdd = wm.add_watch('./mov', mask, rec=True)
+wdd = wm.add_watch(path, mask, rec=True)
 
 notifier.loop()

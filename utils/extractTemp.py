c3269de0a9f8574f1ec0a80112437378b829799b
YukunVVan <31962749+YukunVVan@users.noreply.github.com>
2018-06-04 16:37:38 -0400

    update extract temp

--- utils/extractTemp.py
+++ utils/extractTemp.py
@@ -1,27 +1,29 @@
 import pyinotify
 import os
+from w1thermsensor import W1ThermSensor
+from datetime import datetime
 
 # path = "/nfs-volume/volume5/HTI/1mtcNorth"
-path = './mov'
-des = './mov/temp'
+path = '/home/pi/temp'
+dest = '/home/pi/temp'
 wm = pyinotify.WatchManager()  # Watch Manager
-mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE  # watched events
+mask = pyinotify.IN_CREATE  # watched events
 
 class EventHandler(pyinotify.ProcessEvent):
-    def gettemp(self,f):
-        file = open(f,'r')
-        return file.read()
+    # def gettemp(self,f):
+    #     # file = open(f,'r')
+    #     # return file.read()
+    #     for sensor in W1ThermSensor.get_available_sensors():
+    #         print("Sensor %s has temperature %.2f" % (sensor.id, sensor.get_temperature()))
 
     def process_IN_CREATE(self, event):
         if event.pathname.split('.')[-1] != 'temp':
             nf = event.pathname.split('/')[-1].split('.')[0]+'.temp'
             print("Creating: {}".format(event.pathname))
             t = open(os.path.join(des, nf), 'w')
-            t.write(self.gettemp(event.pathname))
+            for sensor in W1ThermSensor.get_available_sensors():
+                t.write("{},{},{}".format(datetime.now().isoformat(),sensor.id,sensor.get_temperature()))
             t.close()
-
-    def process_IN_DELETE(self, event):
-        print("Removing: {}".format(event.pathname))
 
 handler = EventHandler()
 notifier = pyinotify.Notifier(wm, handler)
